d132a9b7ef3a580b687b8b4ba25fc49001129391
abgeorge7 <abgeorge@umich.edu>
2019-07-31 11:43:12 -0500

    Add Canine

--- src/js/commons.js
+++ src/js/commons.js
@@ -24,4 +24,5 @@ $( document ).ready(function() {
     addCommons("acct", "https://acct.bionimbus.org/index/_stats");
     addCommons("anvil", "https://gen3.theanvil.io/index/_stats");
     addCommons("ibdgc", "https://ibdgc.datacommons.io/index/_stats");
+    addCommons("canine", "https://caninedc.org/index/_stats")
 });
