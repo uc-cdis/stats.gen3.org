161b934c79ce75d5533f4cab094948c3843c2945
abgeorge7 <abgeorge@umich.edu>
2019-11-07 14:57:08 -0600

    Add comment

--- src/js/subjectCounts.js
+++ src/js/subjectCounts.js
@@ -1,3 +1,10 @@
+/*
+  We should switch to using the Peregrine /datasets API at some point,
+  but as of now that requires making all projects in a commons publicly
+  available and some commons will not approve that
+*/
+
+
 const subjectCounts = {
   "acct": 829,
   "anvil": 0,
