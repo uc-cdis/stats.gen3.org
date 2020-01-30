106499dd419a8b65d17c077b3e2e24c012de589e
Qingya Shu <qingya.shu@gmail.com>
2020-01-29 19:34:26 -0600

    fix(style): fix font size and emphasize numbers

--- src/js/commons.js
+++ src/js/commons.js
@@ -7,14 +7,21 @@ function addTotals() {
   Object.keys(subjectCounts).forEach((commons) => {
     total += subjectCounts[commons] ? parseInt(subjectCounts[commons].replace(/,/g, '')) : 0;
   });
-  $( ".totals").remove();
-  $( "#header" ).append(
-    "<div class=\"totals\"><p>Total Subjects: " + numberWithCommas(total) +
-    // "</p><p>Total Clinical Attributes: " + numberWithCommas(aggClinicalAttrs) +
-    "</p><p>Total Files: " + numberWithCommas(aggFiles) +
-    "</p><p>Total File Size: " + humanFileSize(aggFileSize) +
-    "</p></div>"
-  );
+  $( ".total-count-card").remove();
+  $( "#header" ).append(`
+    <div class="total-count-card">
+      <div class="total-count-card__number">${numberWithCommas(total)}</div>
+      <div class="total-count-card__text">Total Subjects</div>
+    </div>
+    <div class="total-count-card">
+      <div class="total-count-card__number">${numberWithCommas(aggFiles)}</div>
+      <div class="total-count-card__text">Total Files</div>
+    </div>
+    <div class="total-count-card">
+      <div class="total-count-card__number">${humanFileSize(aggFileSize)}</div>
+      <div class="total-count-card__text">Total File Size</div>
+    </div>
+  `);
 }
 
 function numberWithCommas(str) {
@@ -28,7 +35,27 @@ function humanFileSize(size) {
     return `${sizeStr} ${suffix}`;
 }
 
-function addCommons(abbv, indexdEndpoint, dictionaryEndpoint) {
+function getCommonHTML(commonAbbv, logoHrefLink, subjectCount, clinicalAttributeCount, indexdFileCount, indexdFileSize) {
+  return `
+  <div class="card common-card text-center">
+    <div>
+      <a href="${logoHrefLink}" target="_blank" class="common-card__logo-wrapper">
+        <img src="logos/${commonAbbv}.png" class="card-img-top common-card__logo" alt="...">
+      </a>
+    </div>
+    <div class="card-body">
+      <p class="card-text">
+        ${subjectCount ? `<p class="common-card__info"><span class="common-card__number col-6 common-card__text--left">${subjectCount}</span><span class="col-6 common-card__text--right"> Subjects</span></p>` : ''}
+        <p class="common-card__info"><span class="common-card__number col-6 common-card__text--left">${clinicalAttributeCount.toLocaleString()}</span><span class="col-6 common-card__text--right"> Attributes</span></p>
+        <p class="common-card__info"><span class="common-card__number col-6 common-card__text--left">${indexdFileCount.toLocaleString()}</span><span class="col-6 common-card__text--right"> Files</span></p>
+        <p class="common-card__info"><span class="col-6 common-card__text--left">Total Size </span><span class="common-card__number col-6 common-card__text--right">${humanFileSize(indexdFileSize)}</span></p>
+      </p>
+    </div>
+  </div>
+  `;
+}
+
+function addCommons(abbv, logoHrefLink, indexdEndpoint, dictionaryEndpoint) {
   $.getJSON(indexdEndpoint,function(indexdData) {
     $.getJSON(dictionaryEndpoint, function(dictionaryData) {
       let clinicalAttributeCount = 0;
@@ -36,29 +63,35 @@ function addCommons(abbv, indexdEndpoint, dictionaryEndpoint) {
       nodes.forEach((node) => {
         clinicalAttributeCount += Object.keys(dictionaryData[node].properties).length;
       });
-      let subjectCount = subjectCounts[abbv] ? ("<p>Number of Subjects: " + subjectCounts[abbv]) : "";
       aggClinicalAttrs += clinicalAttributeCount;
       aggFiles += indexdData.fileCount;
       aggFileSize += indexdData.totalFileSize;
-      $( "#main" ).append( "<div class=\"commons\"><div class=\"commonsimg\"><img src=\"logos/" + abbv + ".png\" /></div>" + subjectCount + "<p>Number of Attributes: " + clinicalAttributeCount.toLocaleString() + "</p><p>Total Files: " + indexdData.fileCount.toLocaleString() + "</p><p>Total Size: " + humanFileSize(indexdData.totalFileSize) + "</p></div>");
+      $( "#main" ).append(getCommonHTML(
+        abbv,
+        logoHrefLink,
+        subjectCounts[abbv],
+        clinicalAttributeCount,
+        indexdData.fileCount,
+        indexdData.totalFileSize,
+      ));
       addTotals();
     });
   });
 }
 
 $( document ).ready(function() {
-  addCommons("bloodpac", "https://data.bloodpac.org/index/_stats", "https://data.bloodpac.org/api/v0/submission/_dictionary/_all");
-  addCommons("brain", "https://data.braincommons.org/index/_stats", "https://data.braincommons.org/api/v0/submission/_dictionary/_all");
-  addCommons("niaid", "https://niaid.bionimbus.org/index/_stats", "https://niaid.bionimbus.org/api/v0/submission/_dictionary/_all");
-  addCommons("crdc", "https://nci-crdc.datacommons.io/index/_stats", "https://nci-crdc.datacommons.io/api/v0/submission/_dictionary/_all");
-  addCommons("stage", "https://gen3.datastage.io/index/_stats", "https://gen3.datastage.io/api/v0/submission/_dictionary/_all");
-  addCommons("genomel", "https://genomel.bionimbus.org/index/_stats", "https://genomel.bionimbus.org/api/v0/submission/_dictionary/_all");
-  addCommons("edc", "https://portal.occ-data.org/index/_stats", "https://portal.occ-data.org/api/v0/submission/_dictionary/_all");
-  addCommons("kf", "https://data.kidsfirstdrc.org/index/_stats", "https://data.kidsfirstdrc.org/api/v0/submission/_dictionary/_all");
-  addCommons("acct", "https://acct.bionimbus.org/index/_stats", "https://acct.bionimbus.org/api/v0/submission/_dictionary/_all");
-  addCommons("anvil", "https://gen3.theanvil.io/index/_stats", "https://gen3.theanvil.io/api/v0/submission/_dictionary/_all");
-  addCommons("ibdgc", "https://ibdgc.datacommons.io/index/_stats", "https://ibdgc.datacommons.io/api/v0/submission/_dictionary/_all");
-  addCommons("canine", "https://caninedc.org/index/_stats", "https://caninedc.org/api/v0/submission/_dictionary/_all");
-  addCommons("vpodc", "https://vpodc.org/index/_stats", "https://vpodc.org/api/v0/submission/_dictionary/_all");
-  addCommons("pcdc", "https://portal.pedscommons.org/index/_stats", "https://portal.pedscommons.org/api/v0/submission/_dictionary/_all");
+  addCommons("bloodpac", "https://data.bloodpac.org", "https://data.bloodpac.org/index/_stats", "https://data.bloodpac.org/api/v0/submission/_dictionary/_all");
+  addCommons("brain", "https://data.braincommons.org", "https://data.braincommons.org/index/_stats", "https://data.braincommons.org/api/v0/submission/_dictionary/_all");
+  addCommons("niaid", "https://niaid.bionimbus.org", "https://niaid.bionimbus.org/index/_stats", "https://niaid.bionimbus.org/api/v0/submission/_dictionary/_all");
+  addCommons("crdc", "https://nci-crdc.datacommons.io", "https://nci-crdc.datacommons.io/index/_stats", "https://nci-crdc.datacommons.io/api/v0/submission/_dictionary/_all");
+  addCommons("stage", "https://gen3.datastage.io", "https://gen3.datastage.io/index/_stats", "https://gen3.datastage.io/api/v0/submission/_dictionary/_all");
+  addCommons("genomel", "https://genomel.bionimbus.org", "https://genomel.bionimbus.org/index/_stats", "https://genomel.bionimbus.org/api/v0/submission/_dictionary/_all");
+  addCommons("edc", "https://portal.occ-data.org", "https://portal.occ-data.org/index/_stats", "https://portal.occ-data.org/api/v0/submission/_dictionary/_all");
+  addCommons("kf", "https://data.kidsfirstdrc.org", "https://data.kidsfirstdrc.org/index/_stats", "https://data.kidsfirstdrc.org/api/v0/submission/_dictionary/_all");
+  addCommons("acct", "https://acct.bionimbus.org", "https://acct.bionimbus.org/index/_stats", "https://acct.bionimbus.org/api/v0/submission/_dictionary/_all");
+  addCommons("anvil", "https://gen3.theanvil.io", "https://gen3.theanvil.io/index/_stats", "https://gen3.theanvil.io/api/v0/submission/_dictionary/_all");
+  addCommons("ibdgc", "https://ibdgc.datacommons.io", "https://ibdgc.datacommons.io/index/_stats", "https://ibdgc.datacommons.io/api/v0/submission/_dictionary/_all");
+  addCommons("canine", "https://caninedc.org", "https://caninedc.org/index/_stats", "https://caninedc.org/api/v0/submission/_dictionary/_all");
+  addCommons("vpodc", "https://vpodc.org", "https://vpodc.org/index/_stats", "https://vpodc.org/api/v0/submission/_dictionary/_all");
+  addCommons("pcdc", "https://portal.pedscommons.org", "https://portal.pedscommons.org/index/_stats", "https://portal.pedscommons.org/api/v0/submission/_dictionary/_all");
 });
