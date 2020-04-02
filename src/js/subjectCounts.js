faebe772b6ce9405d1d36287bc837bb5d08ee275
Gina Kuffel <ginakuffel@MacBook-Pro-3.local>
2020-04-02 12:53:53 -0500

    Changed number of subject in COVID commons

--- src/js/subjectCounts.js
+++ src/js/subjectCounts.js
@@ -11,7 +11,7 @@ const subjectCounts = {
   "bloodpac": "4,839",
   "brain": "7,175",
   "canine": "1,499",
-  "covid19": "0",
+  "covid19": "1085",
   "crdc": "83,709",
   "edc": null,
   "genomel": "1,390",
