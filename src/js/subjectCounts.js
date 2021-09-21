7a0d89ea66714471e4791809b8fbc972ea8ce948
kthyatt <79868106+kthyatt@users.noreply.github.com>
2021-09-21 16:00:43 -0500

    Update subjectCounts.js

--- src/js/subjectCounts.js
+++ src/js/subjectCounts.js
@@ -16,7 +16,7 @@ const subjectCounts = {
   "genomel": "1,390",
   "ibdgc": "107,418",
   "kf": "21,833",
-  "midrc": "1325",
+  "midrc": "5,356",
   "niaid": "48,268",
   "pcdc": "20,446",
   "stage": "240,460",
