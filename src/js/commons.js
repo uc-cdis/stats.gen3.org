1f76df364ff654a3bbb7296bd7132b3bb8639014
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2021-11-15 18:39:29 -0600

    feat(VA): Added vhdc to stats page

--- src/js/commons.js
+++ src/js/commons.js
@@ -111,4 +111,5 @@ $( document ).ready(function() {
   addCommons("oadc", "https://gen3.datacommons.io/", "https://gen3.datacommons.io//index/_stats", "https://gen3.datacommons.io/api/v0/submission/_dictionary/_all", "Open Access Data Commons");
   addCommons("jcoin", "https://jcoin.datacommons.io/", "https://jcoin.datacommons.io/index/_stats", "https://jcoin.datacommons.io/api/v0/submission/_dictionary/_all")
   addCommons("va", "https://va.data-commons.org/", "https://va.data-commons.org/index/_stats", "https://va.data-commons.org/api/v0/submission/_dictionary/_all")
+  addCommons("heal", "https://healdata.org/", "https://healdata.org/index/_stats", "https://healdata.org/api/v0/submission/_dictionary/_all")
 });
