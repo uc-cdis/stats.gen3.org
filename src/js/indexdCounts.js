aaee7a699c6ca78770600e3cbdd83d7aeb8c2fea
Pauline <4224001+paulineribeyre@users.noreply.github.com>
2023-11-13 15:39:38 -0700

    Update indexd counts

--- src/js/indexdCounts.js
+++ src/js/indexdCounts.js
@@ -10,7 +10,7 @@ const indexdCountsCache = {
   "nct": { fileCount: 10, totalFileSize: 3883267 },
   "oadc": { fileCount: 1605, totalFileSize: 13773567905048 },
   "jcoin": { fileCount: 464, totalFileSize: 1540606213 },
-  "va": { fileCount: 7640, totalFileSize: 4101940731150 },
+  "va": { fileCount: 8575, totalFileSize: 4278104651236 },
   "bloodpac": { fileCount: 35569, totalFileSize: 34663094742719 },
   "genomel": { fileCount: 6555, totalFileSize: 31600413892340 },
   "canine": { fileCount: 3820, totalFileSize: 1884253865578 },
@@ -20,7 +20,7 @@ const indexdCountsCache = {
   "covid19": { fileCount: 285849, totalFileSize: 117637133283369 },
   "bdc": { fileCount: 749297, totalFileSize: 4000909355592939 },
   "vpodc": { fileCount: 352786, totalFileSize: 2184859714735 },
-  "kf": { fileCount: 1198736, totalFileSize: 7261751379232888 },
-  "midrc": { fileCount: 3709533, totalFileSize: 13599327337305 },
-  "crdc": {fileCount: 52873264, totalFileSize: 9849559206241160}
+  "kf": { fileCount: 1201691, totalFileSize: 7264720100478774 },
+  "midrc": { fileCount: 3722994, totalFileSize: 13928280076013 },
+  "crdc": {fileCount: 52874987, totalFileSize: 9861313737996044}
 };
