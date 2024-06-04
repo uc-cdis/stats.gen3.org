2aba722833f0aac122124ae3e3ad95f830771eaf
root <root@LAPTOP-Q6VEA4LK>
2024-06-04 15:48:20 -0500

    added github workflow

---
+++ update.py
@@ -0,0 +1,45 @@
+import json
+import requests
+
+try:
+    inst_file = open("./src/js/instances.json")
+    instances = json.load(inst_file)
+except:
+    print("ERROR: instances.json not found.")
+    exit(1)
+try:
+    subj_file = open("./src/js/subjectCounts.json", "r+")
+    subject_counts = json.load(subj_file)
+except:
+    print("ERROR: subjectCounts.json not found.")
+    exit(1)
+try:
+    indx_file = open("./src/js/indexdCounts.json", "r+")
+    indexd_counts = json.load(indx_file)
+except:
+    print("ERROR: indexdCounts.json not found.")
+    exit(1)
+
+for instance, vals in instances.items():
+    resp = requests.get(vals["file_stats_endpoint"])
+    if resp.status_code == 200:
+        indexd_counts[instance] = resp.json()
+    if "subject_stats_endpoint" in vals:
+        resp = requests.get(vals["subject_stats_endpoint"])
+        if resp.status_code == 200:
+            subj_count = 0
+            subjects = resp.json()
+            for key, val in subjects.items():
+                for k, v in val.items():
+                    subj_count += v
+            subject_counts[instance] = subj_count
+
+indx_file.seek(0)
+json.dump(indexd_counts, indx_file)
+indx_file.truncate()
+indx_file.close()
+subj_file.seek(0)
+json.dump(subject_counts, subj_file)
+subj_file.truncate()
+subj_file.close()
+inst_file.close()
