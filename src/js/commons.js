3215c44e7e8003a9f1805a05bb5168cccd37ee99
michaelfitzo <fitz@uchicago.edu>
2024-07-10 10:36:07 -0500

    add prometheus

--- src/js/commons.js
+++ src/js/commons.js
@@ -105,7 +105,6 @@ function addAggCommons(commonAbbv, logoHrefLink, description, repos, title = "")
         <p class=common-card__title>${title}</p>
         <p class=mesh__description>${description}</p>
         <p class=common-card__info>Data Repositories:<span class="common-card__number"> ${repos.toLocaleString()}</span></p>
-
       </div>
     </div>
   </div>
@@ -115,9 +114,10 @@ function addAggCommons(commonAbbv, logoHrefLink, description, repos, title = "")
 
 $(document).ready(async function () {
   // meshes
-  addAggCommons("bdf", "https://imaging-hub.data-commons.org/Explorer", "The MIDRC BDF Imaging Hub (BIH) allows researchers to query and analyze data from independent data repositories or resources related to medical imaging.", 3)
-  addAggCommons("heal", "https://healdata.org/", "The HEAL Data Platform enables search and discovery across multiple data repositories supporting the Helping to End Addiction Long-term (HEAL) Initiative.", 9)
-  addAggCommons("brh", "https://brh.data-commons.org/", "The Biomedical Research Hub enables search, discovery and the analysis of data from data commons from across NIH Institutes, Centers and projects.", 11)
+  addAggCommons("bdf", "https://imaging-hub.data-commons.org/Explorer", "The MIDRC BDF Imaging Hub (BIH) allows researchers to query and analyze data from independent data repositories or resources related to medical imaging.<br> </br>", 3)
+  addAggCommons("heal", "https://healdata.org/", "The HEAL Data Platform enables search and discovery across multiple data repositories supporting the Helping to End Addiction Long-term (HEAL) Initiative.<br> </br>", 9)
+  addAggCommons("brh", "https://brh.data-commons.org/", "The Biomedical Research Hub enables search, discovery and the analysis of data from data commons from across NIH Institutes, Centers and projects. <br> </br> <br>", 11)
+  addAggCommons("prom", "https://prometheus.data-commons.org/", "PROMETHEUS is the Project for Military Exposures and Toxin History Evaluation in US Service Members. The Prometheus Data Platform (PDP) is part of the President Biden Cancer Moonshot 2.0, which aims to mobilize a national effort to end cancer as we know it.", 4)
 
   // commons
   for (let [abbreviation, data] of Object.entries(instances)) {
