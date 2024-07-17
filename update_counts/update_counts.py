36da9cd28c829ecb8f129379509fe7e7c0f00f90
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2024-07-17 11:36:52 -0500

    Handle 'requests.get(url)' error (#102)

--- update_counts/update_counts.py
+++ update_counts/update_counts.py
@@ -38,29 +38,37 @@ def get_total_subject_count(instance_name, subjects):
 
 def main():
     instances = read_instances_file()
-    err_msg = "  ERROR: Unable to update counts for instance '{}' at {}; moving on to the next instance. Details: status code {} - {}"
+    err_msg = "  ERROR: Unable to update counts for instance '{}' at {}; moving on to the next instance. Details: {}"
 
     for instance_name, values in instances.items():
         print(f"INFO: Updating counts for instance '{instance_name}'")
         if "file_stats_endpoint" in values:
             url = values["file_stats_endpoint"]
-            resp = requests.get(url)
+            try:
+                resp = requests.get(url)
+            except Exception as e:
+                print(err_msg.format(instance_name, url, e))
+                continue
             if resp.status_code == 200:
                 data = resp.json()
                 instances[instance_name]["file_count"] = data["fileCount"]
                 instances[instance_name]["total_file_size"] = data["totalFileSize"]
             else:
-                print(err_msg.format(instance_name, url, resp.status_code, resp.text))
+                print(err_msg.format(instance_name, url, f"status code {resp.status_code} - {resp.text}"))
         else:
             print(f"  INFO: Not updating file counts for instance '{instance_name}' because 'file_stats_endpoint' is not configured")
 
         if "subject_stats_endpoint" in values:
             url = values["subject_stats_endpoint"]
-            resp = requests.get(url)
+            try:
+                resp = requests.get(url)
+            except Exception as e:
+                print(err_msg.format(instance_name, url, e))
+                continue
             if resp.status_code == 200:
                 instances[instance_name]["subject_count"] = get_total_subject_count(instance_name, resp.json())
             else:
-                print(err_msg.format(instance_name, url, resp.status_code, resp.text))
+                print(err_msg.format(instance_name, url, f"status code {resp.status_code} - {resp.text}"))
         else:
             print(f"  INFO: Not updating subject count for instance '{instance_name}' because 'subject_stats_endpoint' is not configured")
 
