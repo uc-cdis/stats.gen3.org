09b10e647d35e84841377d90236367cf4b744382
paulineribeyre <paulineribeyre@users.noreply.github.com>
2024-08-23 05:04:44 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -14,8 +14,8 @@ const instances = {
         "dictionary_endpoint": "https://data.bloodpac.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://data.bloodpac.org/index/_stats",
         "subject_stats_endpoint": "https://data.bloodpac.org/api/search/datasets?nodes=case",
-        "file_count": 35679,
-        "total_file_size": 34946195172111,
+        "file_count": 35680,
+        "total_file_size": 34948772793614,
         "subject_count": 4441
     },
     "canine": {
@@ -75,8 +75,8 @@ const instances = {
         "dictionary_endpoint": "https://data.kidsfirstdrc.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://data.kidsfirstdrc.org/index/_stats",
         "subject_stats_endpoint": "https://kf-api-arranger-next.kf-strides.org/statistics",
-        "file_count": 2044092,
-        "total_file_size": 7864588986400232,
+        "file_count": 2145850,
+        "total_file_size": 7870801827793312,
         "subject_count": 28670
     },
     "midrc": {
@@ -118,8 +118,8 @@ const instances = {
         "logo_link": "https://va.data-commons.org/",
         "dictionary_endpoint": "https://va.data-commons.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://va.data-commons.org/index/_stats",
-        "file_count": 14709,
-        "total_file_size": 2496185104469,
+        "file_count": 14744,
+        "total_file_size": 2500928949273,
         "subject_count": 648242
     },
     "vpodc": {
