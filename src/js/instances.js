1e343285621d81408f69dd59a187ff08e68a1260
paulineribeyre <paulineribeyre@users.noreply.github.com>
2024-09-04 05:05:43 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -14,8 +14,8 @@ const instances = {
         "dictionary_endpoint": "https://data.bloodpac.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://data.bloodpac.org/index/_stats",
         "subject_stats_endpoint": "https://data.bloodpac.org/api/search/datasets?nodes=case",
-        "file_count": 35863,
-        "total_file_size": 35507909290814,
+        "file_count": 35891,
+        "total_file_size": 35541376128818,
         "subject_count": 4441
     },
     "canine": {
@@ -118,8 +118,8 @@ const instances = {
         "logo_link": "https://va.data-commons.org/",
         "dictionary_endpoint": "https://va.data-commons.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://va.data-commons.org/index/_stats",
-        "file_count": 15299,
-        "total_file_size": 2595264844748,
+        "file_count": 15319,
+        "total_file_size": 2599079357287,
         "subject_count": 648242
     },
     "vpodc": {
