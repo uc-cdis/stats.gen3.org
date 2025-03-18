0bf02046ab0b979cb083d25c59078661e4b2abbb
michaelfitzo <116322184+michaelfitzo@users.noreply.github.com>
2025-03-18 18:33:17 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -57,8 +57,8 @@ const instances = {
         "dictionary_endpoint": "https://data.kidsfirstdrc.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://data.kidsfirstdrc.org/index/_stats",
         "subject_stats_endpoint": "https://kf-api-arranger-next.kf-strides.org/statistics",
-        "file_count": 2688869,
-        "total_file_size": 8122958233508313,
+        "file_count": 2689345,
+        "total_file_size": 8123564719491852,
         "subject_count": 28670
     },
     "midrc": {
