61deb001b151b3dfb22ddcaab61ec804378bafe5
michaelfitzo <116322184+michaelfitzo@users.noreply.github.com>
2025-04-01 19:10:31 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -84,9 +84,9 @@ const instances = {
         "dictionary_endpoint": "https://gen3.biodatacatalyst.nhlbi.nih.gov/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://gen3.biodatacatalyst.nhlbi.nih.gov/index/_stats",
         "subject_stats_endpoint": "https://gen3.biodatacatalyst.nhlbi.nih.gov/api/search/datasets?nodes=subject",
-        "file_count": 1583907,
-        "total_file_size": 4298742263151415,
-        "subject_count": 1117695
+        "file_count": 1600425,
+        "total_file_size": 4446192539090251,
+        "subject_count": 1130330
     },
     "va": {
         "logo_link": "https://va.data-commons.org/",
