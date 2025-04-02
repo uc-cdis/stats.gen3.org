75de05984bc62d69cc4236d0f17c73441517c2eb
paulineribeyre <4224001+paulineribeyre@users.noreply.github.com>
2025-04-02 05:04:36 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -84,8 +84,8 @@ const instances = {
         "dictionary_endpoint": "https://gen3.biodatacatalyst.nhlbi.nih.gov/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://gen3.biodatacatalyst.nhlbi.nih.gov/index/_stats",
         "subject_stats_endpoint": "https://gen3.biodatacatalyst.nhlbi.nih.gov/api/search/datasets?nodes=subject",
-        "file_count": 1600425,
-        "total_file_size": 4446192539090251,
+        "file_count": 1600933,
+        "total_file_size": 4446194604652722,
         "subject_count": 1130330
     },
     "va": {
