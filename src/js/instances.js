f4056d97bc51726d0f712b4f897296929aa2f2b1
paulineribeyre <4224001+paulineribeyre@users.noreply.github.com>
2025-07-02 05:06:33 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -57,8 +57,8 @@ const instances = {
         "dictionary_endpoint": "https://data.kidsfirstdrc.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://data.kidsfirstdrc.org/index/_stats",
         "subject_stats_endpoint": "https://kf-api-arranger-next.kf-strides.org/statistics",
-        "file_count": 2882762,
-        "total_file_size": 8216260257143468,
+        "file_count": 2887340,
+        "total_file_size": 8224130475041451,
         "subject_count": 28670
     },
     "midrc": {
