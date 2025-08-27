dc169dea600a59eb386beadb89d62725367276d8
paulineribeyre <4224001+paulineribeyre@users.noreply.github.com>
2025-08-27 05:05:13 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -23,15 +23,15 @@ const instances = {
         "subject_stats_endpoint": "https://chicagoland.pandemicresponsecommons.org/api/search/datasets?nodes=subject",
         "file_count": 285849,
         "total_file_size": 117637133283369,
-        "subject_count": 598675
+        "subject_count": 657775
     },
     "crdc": {
         "logo_link": "https://nci-crdc.datacommons.io",
         "dictionary_endpoint": "https://nci-crdc.datacommons.io/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://nci-crdc.datacommons.io/index/_stats",
         "subject_stats_endpoint": "https://api.gdc.cancer.gov/projects?facets=summary.case_count",
-        "file_count": 58740965,
-        "total_file_size": 16126289508476889,
+        "file_count": 58793523,
+        "total_file_size": 16241579291230600,
         "subject_count": 45087
     },
     "icgc": {
@@ -57,8 +57,8 @@ const instances = {
         "dictionary_endpoint": "https://data.kidsfirstdrc.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://data.kidsfirstdrc.org/index/_stats",
         "subject_stats_endpoint": "https://kf-api-arranger-next.kf-strides.org/statistics",
-        "file_count": 2924409,
-        "total_file_size": 8291250336914096,
+        "file_count": 2928442,
+        "total_file_size": 8339559124747370,
         "subject_count": 28670
     },
     "midrc": {
