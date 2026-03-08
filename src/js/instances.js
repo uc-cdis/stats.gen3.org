3a2f8ea4473aabfe74ef5835d87cf6e21cddb9f4
paulineribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-03-08 05:21:13 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -108,8 +108,8 @@ const instances = {
         "logo_link": "https://vpodc.data-commons.org",
         "dictionary_endpoint": "https://vpodc.data-commons.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://vpodc.data-commons.org/index/_stats",
-        "file_count": 424072,
-        "total_file_size": 2221988749881,
+        "file_count": 424112,
+        "total_file_size": 2222016803062,
         "subject_count": 163695
     }
 }
