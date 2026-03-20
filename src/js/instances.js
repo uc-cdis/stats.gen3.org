36ae70d4b91c180bb6828b62ad785d212026ba26
paulineribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-03-20 05:26:19 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -30,8 +30,8 @@ const instances = {
         "dictionary_endpoint": "https://nci-crdc.datacommons.io/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://nci-crdc.datacommons.io/index/_stats",
         "subject_stats_endpoint": "https://api.gdc.cancer.gov/projects?facets=summary.case_count",
-        "file_count": 60427430,
-        "total_file_size": 18332496519869253,
+        "file_count": 60433878,
+        "total_file_size": 18453652912129586,
         "subject_count": 50270
     },
     "icgc": {
@@ -57,8 +57,8 @@ const instances = {
         "dictionary_endpoint": "https://data.kidsfirstdrc.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://data.kidsfirstdrc.org/index/_stats",
         "subject_stats_endpoint": "https://kf-api-arranger-next.kf-strides.org/statistics",
-        "file_count": 3724140,
-        "total_file_size": 9664893098595594,
+        "file_count": 3724199,
+        "total_file_size": 9665713702150254,
         "subject_count": 28670
     },
     "midrc": {
@@ -108,8 +108,8 @@ const instances = {
         "logo_link": "https://vpodc.data-commons.org",
         "dictionary_endpoint": "https://vpodc.data-commons.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://vpodc.data-commons.org/index/_stats",
-        "file_count": 424851,
-        "total_file_size": 2257891015795,
+        "file_count": 424860,
+        "total_file_size": 2257899551917,
         "subject_count": 163695
     }
 }
