7283a70967af2e469323cba70051ebe03c07277c
paulineribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-08-21 05:10:52 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -57,8 +57,8 @@ const instances = {
         "dictionary_endpoint": "https://data.kidsfirstdrc.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://data.kidsfirstdrc.org/index/_stats",
         "subject_stats_endpoint": "https://kf-api-arranger-next.kf-strides.org/statistics",
-        "file_count": 3797550,
-        "total_file_size": 9845967267977258,
+        "file_count": 3798676,
+        "total_file_size": 9856061220651753,
         "subject_count": 28670
     },
     "midrc": {
@@ -92,9 +92,9 @@ const instances = {
         "dictionary_endpoint": "https://gen3.biodatacatalyst.nhlbi.nih.gov/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://gen3.biodatacatalyst.nhlbi.nih.gov/index/_stats",
         "subject_stats_endpoint": "https://gen3.biodatacatalyst.nhlbi.nih.gov/api/search/datasets?nodes=subject",
-        "file_count": 2667914,
-        "total_file_size": 5013880477321061,
-        "subject_count": 2755071
+        "file_count": 2675943,
+        "total_file_size": 5031912082348914,
+        "subject_count": 2793648
     },
     "va": {
         "logo_link": "https://va.data-commons.org/",
