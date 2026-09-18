a874d97b0da3f5c4af1ca5c8cc2d87f9aa9204a1
paulineribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-09-18 05:15:07 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -31,7 +31,7 @@ const instances = {
         "file_stats_endpoint": "https://nci-crdc.datacommons.io/index/_stats",
         "subject_stats_endpoint": "https://api.gdc.cancer.gov/projects?facets=summary.case_count",
         "file_count": 71143419,
-        "total_file_size": 19576602198576290,
+        "total_file_size": 19576609248307086,
         "subject_count": 50571
     },
     "icgc": {
